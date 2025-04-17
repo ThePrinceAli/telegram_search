806b719b116fbb3db462762059532aadcc94565c
ThePrinceAli <hackerali070@gmail.com>
2025-04-17 08:09:41 +0500

    add

--- index.js
+++ index.js
@@ -7,11 +7,8 @@ const animalsOptions = {
     reply_markup: {
         inline_keyboard: [
             [
-                { text: 'Fox', callback_data: 'fox' },
-                { text: 'Tiger', callback_data: 'tiger' },
-                { text: 'Lion', callback_data: 'lion' },
-                { text: 'Bear', callback_data: 'bear' }
-
+                { text: 'Random Img', callback_data: 'random' },
+                { text: 'Img by ID', callback_data: 'byid' }
             ]
         ]
     }
@@ -19,19 +16,21 @@ const animalsOptions = {
 
 
 bot.on("message", async (msg) => {
-  const text = msg.text;
-  const chatId = msg.chat.id;
-  if (text === "/start") {
-    await bot.sendMessage(chatId, "Bot ish boshladi",animalsOptions);
-  }
+    const text = msg.text;
+    const chatId = msg.chat.id;
+    if (text === "/start") {
+        await bot.sendMessage(chatId, "Bot ish boshladi", animalsOptions);
+    }
 });
-bot.on('callback_query',async (msg)=>{
-    const data=msg.data
-    const chatId = msg.message.chat.id;  
-        fetch(`${process.env.API_KEY}${data}`).then(res=>res.json()).then(data=>
-     data.map(async (item,index)=>{
-         await bot.sendMessage(chatId, `Title: ${item.name}
-            Family: ${item.taxonomy.family}
-            `)
-     }))
+bot.on('callback_query', async (msg) => {
+    const data = msg.data
+    const chatId = msg.message.chat.id;
+    const randomNumber=Math.floor(Math.random()*100)
+    if(data=='random'){ 
+        fetch(process.env.API_KEY).then(res => res.json()).then(data =>
+            bot.sendPhoto(chatId, data[randomNumber].download_url,animalsOptions)
+        )
+            
+    }
+// console.log(data) 
 })
